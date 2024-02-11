e629574c846010acb01bdd801d75d6734a5a2b53
Nyakio-Eva <evalkariuki@gmail.com>
2024-02-11 07:05:29 +0300

    used react v6

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-function App() {
-  return <Home />;
-}
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { BrowserRouter} from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
