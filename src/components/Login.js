da80e81c4dd7b3191d77759c7509ba63307b75a1
Nyakio-Eva <evalkariuki@gmail.com>
2024-02-11 08:04:55 +0300

    added navlink , completed assignment

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,15 @@ function Login() {
     return (
       <div>
         <h1>Login</h1>
+        <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
       </div>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
 
 function NavBar(){
     return (
       <>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/login">Login</Link>
+       <NavLink to="/" exact="true" style={linkStyles} activestyle={{background: "darkblue",}}>Home</NavLink>
+        <Link to="/about" style={linkStyles}>About</Link>
+        <Link to="/login" style={linkStyles}>Login</Link>
       </>
     )
 
